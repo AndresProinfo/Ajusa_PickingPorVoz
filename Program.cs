4e1c73d8c3f140b7765565efdc3c7d5a20a72df1
Andres Castilla <acastilla@proceso.info>
2015-01-09 01:20:03 +0100

    aa

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace VoiceDispatcher
 	static class Program
 	{
 		/// <summary>
-		/// Punto de entrada principal para la aplicación
+		/// Punto de entrada principal para la aplicación.
 		/// </summary>
 		[STAThread]
 		static void Main()
